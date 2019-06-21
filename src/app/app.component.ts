fa453018388ecee550793a0a7ef80c62fd8b2678
Fernando <fernando@MacBook-Pro-de-Fernando.local>
2019-06-21 00:17:20 -0500

    Proyecto inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  file: any = null
+  fileInput = null
   title = 'cloudFunctionsClient';
+  
+  
+  
+  verificar()
+  {
+    if(this.file != null)
+    {
+      alert('Ok')
+    }
+    else
+    {
+      alert('No has subido una imagen')
+    }
+  }
+  
+  onFileSelected(event)
+  {
+    if(event.target.files.length > 0) 
+    {
+      const reader = new FileReader()
+      reader.onload = (e:any) => {
+        this.file = e.target.result
+      }
+      reader.readAsDataURL(event.target.files[0])
+      console.log(event.target.files[0]);
+    }
+  }
 }
